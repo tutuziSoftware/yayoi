86839a5e857cf37c4e38b1e9885d05a1a5170a10
ゆきかご <puruhime@gmail.com>
2014-07-10 00:36:50 +0900

    とりあえずUIが操作出来なくなった

--- public/javascripts/battle.js
+++ public/javascripts/battle.js
@@ -44,6 +44,8 @@ function fieldController($scope, $http){
 				console.log("first draw");
 				$scope.field = field;
 				$scope.$apply();
+				$scope.enemyTurn();
+				$scope.$apply();
 			});
 			
 			socket.on('enemy', function(enemyField){
@@ -316,3 +318,17 @@ function fieldController($scope, $http){
 		}
 	}
 }
+
+
+angular.module('yayoi', []).directive('enemyTurn', function(){
+	return function(scope, iElement, iAttrs){
+		console.log('enemy-turn');
+		scope.enemyTurn = function(){
+			console.log('enemy-turn:');
+			console.log(scope.field.turn);
+			if(scope.field.turn === 'enemy turn'){
+				$('button', iElement).attr('disabled', true);
+			}
+		};
+	};
+});
